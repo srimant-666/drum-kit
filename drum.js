e37409260360567bbb4484e09be6ab5ef52f7a0d
sri <123643109+srimant-666@users.noreply.github.com>
2023-12-30 21:52:45 +0530

    Update drum.js

--- drum.js
+++ drum.js
@@ -4,7 +4,7 @@ console.log(numberOfDrumButtons)
 for (let i = 0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         // new Audio('sounds/tom-1.mp3').play()
-        this.style.color="white"
+        // this.style.color="white"
         let buttonInnerHTML = this.innerHTML;
         switch (buttonInnerHTML) {
             case "w":
